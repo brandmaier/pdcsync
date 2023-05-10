265a65cabd5dad4784b4841e66f66351051f098c
Andreas Brandmaier <andy@brandmaier.de>
2023-05-10 16:54:12 +0200

    added lead/lag feature

--- R/getListOfSynchronisedBlocks.R
+++ R/getListOfSynchronisedBlocks.R
@@ -1,12 +1,30 @@
 #' @title Get list of synchronised blocks
 #' @export
-getListOfSynchronisedBlocks <- function(x) {
-  rr <- sync$rr
+getListOfSynchronisedBlocks <- function(x, type="sync") {
+  rr <- x$rr
   
   bool_ts <-
     apply(rr, 2, function(xelem) {
       min(xelem, na.rm = TRUE)
     }) < x$lag_threshold
+  
+  leadlag_ts <-
+    apply(rr, 2, function(xelem) {
+      which.min(xelem)
+    }) - (((nrow(rr)-1)/2)+1)
+  
+  if (type=="lead") {
+    ts <- leadlag_ts>0
+    ts[!bool_ts] <- FALSE
+  } else if (type=="lag") {
+      ts <- leadlag_ts<0
+      ts[!bool_ts] <- FALSE
+  } else if (type=="sync") {
+    ts <- bool_ts
+  } else {
+    stop("Not implemented")
+    return(NA)
+  }
   
   get_consecutive_blocks <- function(bool_list) {
     blocks <- list()
@@ -28,5 +46,5 @@ getListOfSynchronisedBlocks <- function(x) {
     return(blocks)
   }
   
-  get_consecutive_blocks(bool_ts)
+  get_consecutive_blocks(ts)
 }
